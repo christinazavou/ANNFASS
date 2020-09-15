9c41640d881fc4dc61f7113710830247a29e879a
christinazavou <czavou01@gmail.com>
2020-09-15 12:44:30 +0300

    cleanup after format pc

---
+++ software/create_conda_env_with_cuda.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+env_name=$1
+envs_path=$2
+cuda_version=$3
+py_version=$4
+
+conda create --name $env_name python=$py_version
+
+scripts_path=$envs_path/$env_name/etc/conda
+
+# Scripts under this folder are run whenever conda environment is activated
+mkdir -p "$scripts_path/activate.d"
+touch "$scripts_path/activate.d/activate.sh"
+
+# replace argument one of file set_cuda_version.sh with this cuda_version
+sed "s|\$1|${cuda_version}|g" set_cuda_version.sh >> "$scripts_path/activate.d/activate.sh"
+
+# Scripts under this folder are run whenever conda environment is deactivated
+mkdir -p "$scripts_path/deactivate.d"
+touch "$scripts_path/deactivate.d/deactivate.sh"
+
+cat unset_cuda_version.sh >> "$scripts_path/deactivate.d/deactivate.sh"
