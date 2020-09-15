b1b98db8865aaaf60ecbb8e1627fb9ae44bb86b5
christinazavou <czavou01@gmail.com>
2020-09-15 18:18:01 +0300

    fixing anaconda issue

--- software/create_conda_env_with_cuda.sh
+++ software/create_conda_env_with_cuda.sh
@@ -5,6 +5,7 @@ envs_path=$2
 cuda_version=$3
 py_version=$4
 
+
 conda create --name $env_name python=$py_version
 
 scripts_path=$envs_path/$env_name/etc/conda
@@ -20,4 +21,6 @@ sed "s|\$1|${cuda_version}|g" set_cuda_version.sh >> "$scripts_path/activate.d/a
 mkdir -p "$scripts_path/deactivate.d"
 touch "$scripts_path/deactivate.d/deactivate.sh"
 
-cat unset_cuda_version.sh >> "$scripts_path/deactivate.d/deactivate.sh"
+cuda_version=$1
+
+sed "s|\$1|${env_name}|g" unset_cuda_version.sh >> "$scripts_path/deactivate.d/deactivate.sh"
--- software/unset_cuda_version.sh
+++ software/unset_cuda_version.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=$ORIGINAL_LD_LIBRARY_PATH
 export PATH=$ORIGINAL_PATH
+export PATH=$(echo $PATH | sed "s|/home/graphicslab/anaconda3/envs/$1/bin:|/home/graphicslab/anaconda3/bin:|g")
 export CUDA_DIR=$ORIGINAL_CUDA_DIR
 unset ORIGINAL_LD_LIBRARY_PATH
 unset ORIGINAL_PATH
